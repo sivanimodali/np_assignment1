c3d1db7db1cd596976602439c35911391ed28687
sivanimodali <sivanimodali@gmail.com>
2021-01-04 15:41:00 +0100

    chart client

--- servermain.cpp
+++ servermain.cpp
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 /* You will to add includes here */
@@ -24,21 +25,21 @@ using namespace std;
 
 string convertToOpString(string op, float v1, float v2)
 {
-	string s1 = to_string(v1);
-	string s2 = to_string(v2);
-	string s3 = op;
-	string ret = s3 + " " + s1 + " " + s2;
-	return ret;
+  string s1 = to_string(v1);
+  string s2 = to_string(v2);
+  string s3 = op;
+  string ret = s3 + " " + s1 + " " + s2;
+  return ret;
 
 }
 
 string convertToOpString(string op, int v1, int v2)
 {
-	string s1 = to_string(v1);
-	string s2 = to_string(v2);
-	string s3 = op;
-	string ret = s3 + " " + s1 + " " + s2;
-	return ret;
+  string s1 = to_string(v1);
+  string s2 = to_string(v2);
+  string s3 = op;
+  string ret = s3 + " " + s1 + " " + s2;
+  return ret;
 
 }
 
@@ -46,8 +47,8 @@ int main(int argc, char *argv[]){
 
   /* Do more magic */
 
-	srand(time(0));
-	char buffer1[1256], buffer2[1256]; 
+  srand(time(0));
+  char buffer1[1256], buffer2[1256]; 
     int server = socket(AF_INET, SOCK_STREAM, 0); 
     if (server < 0) 
     {
@@ -96,7 +97,7 @@ int main(int argc, char *argv[]){
 
     while (1) 
     { 
-    	printf("Waiting for client .....\n");
+      printf("Waiting for client .....\n");
         int acc = accept(server, (struct sockaddr*) &peer_addr, &addr_size); 
         printf("Connection Established\n"); 
         char ip[INET_ADDRSTRLEN]; 
@@ -106,49 +107,49 @@ int main(int argc, char *argv[]){
         // for finding port number of client 
         printf("Connection established with IP : %s and PORT : %d\n",  ip, ntohs(peer_addr.sin_port)); 
   
-  		char versions[3][50] = {"TEXT TCP 1.0\n", "TEXT TCP 1.1\n", "\n"}; 
-  		for( int i=0;i<3;i++ )
-  		{
-  			strcpy(buffer1, versions[i]); 
-        	send(acc, buffer1, 1256, 0); 
-  		}
-  		recv(acc, buffer2, 1256, 0); 
+      char versions[3][50] = {"TEXT TCP 1.0\n", "TEXT TCP 1.1\n", "\n"}; 
+      for( int i=0;i<3;i++ )
+      {
+        strcpy(buffer1, versions[i]); 
+          send(acc, buffer1, 1256, 0); 
+      }
+      recv(acc, buffer2, 1256, 0); 
         printf("Client : %s", buffer2); 
         cout << "\n";
         vector<string> operations = {"add","fadd","sub","fsub","div","fdiv","mul","fmul"};
-       	int interaction=1;
-  		while(interaction)
-  		{
-  			int rand_op = rand()%8;
-  			string chosen_operation = operations[rand_op];
-  			// cout << chosen_operation << '\n';
-  			if( chosen_operation[0] == 'f' )
-  			{
-  				// floating point operation
-  				float A = rand()%LIMIT;
-  				float B = rand()%100;
-  				float v1 = A/B;
+        int interaction=1;
+      while(interaction)
+      {
+        int rand_op = rand()%8;
+        string chosen_operation = operations[rand_op];
+        // cout << chosen_operation << '\n';
+        if( chosen_operation[0] == 'f' )
+        {
+          // floating point operation
+          float A = rand()%LIMIT;
+          float B = rand()%100;
+          float v1 = A/B;
 
-  				A = rand()%LIMIT;
-  				B = rand()%100;
-  				float v2 = A/B;
-  				// cout << v1 << " " << v2 << "\n";
-  				float actualAns;
-  				if( chosen_operation == "fadd" )
-  					actualAns = v1+v2;
-  				if( chosen_operation == "fsub" )
-  					actualAns = v1-v2;
-  				if( chosen_operation=="fmul" )
-  					actualAns = v1*v2;
-  				if( chosen_operation=="fdiv" )
-  				{
-  					// no divison of 0 is allowed
-  					if((int)v2 == 0)
-  						v2+=1;
-  					actualAns = v1/v2;
-  				}
-  				// cout << v1 << " " << v2 << " " << actualAns<< "\n";
-  				string sendOpString = convertToOpString(chosen_operation, v1, v2);
+          A = rand()%LIMIT;
+          B = rand()%100;
+          float v2 = A/B;
+          // cout << v1 << " " << v2 << "\n";
+          float actualAns;
+          if( chosen_operation == "fadd" )
+            actualAns = v1+v2;
+          if( chosen_operation == "fsub" )
+            actualAns = v1-v2;
+          if( chosen_operation=="fmul" )
+            actualAns = v1*v2;
+          if( chosen_operation=="fdiv" )
+          {
+            // no divison of 0 is allowed
+            if((int)v2 == 0)
+              v2+=1;
+            actualAns = v1/v2;
+          }
+          // cout << v1 << " " << v2 << " " << actualAns<< "\n";
+          string sendOpString = convertToOpString(chosen_operation, v1, v2);
           char send_str[100];
           int j = 0;
           for(int i=0;i<sendOpString.length();i++)
